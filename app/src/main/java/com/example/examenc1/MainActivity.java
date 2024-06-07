ed7d536e28a29455eeb357a352b547f665cc45c3
Jon Reyes <jonath.lt3@gmail.com>
2024-06-07 12:40:52 -0700

    Se realizo el MainActivity

--- app/src/main/java/com/example/examenc1/MainActivity.java
+++ app/src/main/java/com/example/examenc1/MainActivity.java
@@ -9,16 +9,39 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText usuarioEditText;
+    private EditText contraseñaEditText;
+    private Button ingresarButton;
+    private String usuarioValido;
+    private String contraseñaValida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        usuarioEditText = findViewById(R.id.usuarioEditText);
+        contraseñaEditText = findViewById(R.id.contraseñaEditText);
+        ingresarButton = findViewById(R.id.ingresarButton);
+
+        // Obtener las cadenas definidas en strings.xml
+        usuarioValido = getString(R.string.user);
+        contraseñaValida = getString(R.string.pass);
+
+        ingresarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usuarioIngresado = usuarioEditText.getText().toString();
+                String contraseñaIngresada = contraseñaEditText.getText().toString();
+
+                if (usuarioIngresado.equals(usuarioValido) && contraseñaIngresada.equals(contraseñaValida)) {
+                    Intent intent = new Intent(MainActivity.this, CuentaBancoActivity.class);
+                    startActivity(intent);
+                } else {
+                    // Mostrar mensaje de error
+                    Toast.makeText(MainActivity.this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
