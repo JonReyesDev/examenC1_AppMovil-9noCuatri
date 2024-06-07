4dd41946b6b5bee85b4246e72fc26ca1cfb5f781
Jon Reyes <jonath.lt3@gmail.com>
2024-06-07 12:29:17 -0700

    Se creo la clase CuentaBanco

---
+++ app/src/main/java/com/example/examenc1/CuentaBanco.java
@@ -0,0 +1,34 @@
+package com.example.examenc1;
+
+public class CuentaBanco {
+    private String numeroCuenta;
+    private String nombre;
+    private String banco;
+    private double saldo;
+
+    public CuentaBanco(String numeroCuenta, String nombre, String banco, double saldo) {
+        this.numeroCuenta = numeroCuenta;
+        this.nombre = nombre;
+        this.banco = banco;
+        this.saldo = saldo;
+    }
+
+    public double consultarSaldo() {
+        return saldo;
+    }
+
+    public void depositar(double cantidad) {
+        if (cantidad > 0) {
+            saldo += cantidad;
+        }
+    }
+
+    public boolean retirar(double cantidad) {
+        if (cantidad > 0 && cantidad <= saldo) {
+            saldo -= cantidad;
+            return true;
+        }
+        return false;
+    }
+}
+
